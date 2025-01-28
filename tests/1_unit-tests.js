df744d90501557d6be73c76c5a734a8df8014d70
bpouser01 <63763228+bpouser01@users.noreply.github.com>
2025-01-28 13:24:09 +0900

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -22,8 +22,8 @@ suite('Unit Tests', function () {
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isTrue(true, 'true is true');
+      assert.isNotTrue(!!'double negation', 'Double negation of a truthy value is true');
       assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
